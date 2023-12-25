dcd328b5459346843c0b4b969c2447ba806e11e2
nurulislamrimon <nurulislamrimon@gmail.com>
2023-12-25 17:26:59 +0600

    file structure created

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello");
+import express from "express";
+import http from "http";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 5000;
+
+const root_dir = dirname(fileURLToPath(import.meta.url));
+
+const indexHtml = root_dir + "/ejs/index.html";
+
+const server = http.createServer(app);
+app.get("/", (req, res) => {
+  res.sendFile(indexHtml);
+  //   res.send({ result: "success" });
+});
+
+server.listen(port, () => console.log(`Server is listening at port: ${port}`));
